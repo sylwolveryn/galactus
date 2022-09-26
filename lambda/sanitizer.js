62bcc4a9c7abfb099f839d412418951b0b7a463f
Marcell Neszt <Marcell_Neszt@epam.com>
2022-09-26 22:47:41 +0200

    pages

--- lambda/sanitizer.js
+++
@@ -1,6 +0,0 @@
-const truncate = ( string ) => string.length > 25 ? string.slice(0, 25) : string
-const sanitize = ( string ) => truncate(string.replace(/^a-z0-9\s/gi, ''))
-
-module.exports = {
-    sanitize
-}
