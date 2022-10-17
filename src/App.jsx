c2bdc8b0b3c5cc70881e08703bb6e1addd797a39
Marcell Neszt <Marcell_Neszt@epam.com>
2022-10-17 12:58:14 +0200

    js crash course gamification

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,28 @@ function App() {
     const [name, setName] = createSignal('')
     const [eha, setEha] = createSignal('')
     const [code, setCode] = createSignal('')
+    const [errorMessage, setErrorMessage] = createSignal('')
+    const [errorRetry, setErrorRetry] = createSignal(0)
 
     const handleSubmit = async ( event ) => {
         event.preventDefault()
 
         const rawResponse = await fetch('https://d2gixz63tranhqsldnfpo6hqwi0lgejn.lambda-url.us-east-1.on.aws/', {
             method: 'POST',
-            body: JSON.stringify({
+            body: {
                 name: name(),
                 eha: eha(),
                 code: code(),
-            })
+            }
         })
         const response = await rawResponse.json()
         console.log(response)
+        if (response.indexOf('Teapot') >= 0) {
+            setErrorRetry(errorRetry()+1)
+            errorRetry() > 3
+                ? setErrorMessage('Try to write your own async function in the console: var handleSubmit = async () { .... don\'t forget to set the body as a JSON. Or use Postman, or curl, or... butterflies.}')
+                : setErrorMessage('Ohh no, the handleSubmit function is missing the JSON.stringify api call.. it won\'t be able to reach out to Galactus service!')
+        }
     }
 
     const handleNameUpdate = (value) => setName(value)
@@ -30,6 +38,9 @@ function App() {
             <header class={ styles.header }>
                 <h2>
                     Welcome to Galactus Service
+                </h2>
+                <h2>
+                    {errorMessage()}
                 </h2>
                 <form onSubmit={ handleSubmit }>
                     <label for='name'>Név</label><br/>
@@ -46,10 +57,10 @@ function App() {
                            onChange={(event) => { handleEhaUpdate(event.currentTarget.value) } }
                     ></input><br/>
 
-                    <label for='code'>kód</label><br/>
+                    <label for='code'>Kód</label><br/>
                     <input type='text'
                            id='code'
-                           placeholder='Gwenpool'
+                           placeholder='Wakanda'
                            onChange={(event) => { handleCodeUpdate(event.currentTarget.value) } }
                     ></input><br/>
 
